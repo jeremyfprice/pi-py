b85a93bdfd2ad8176e8afb25629df71dccfa014b
Jeremy Price <jfprice@iupui.edu>
2017-10-25 10:15:30 -0400

    Add files via upload

--- sensor_test2.py
+++ sensor_test2.py
@@ -82,6 +82,6 @@ mediandMQ5 = medianMQ5/1024
 #print " "
 #round(mean(arrayMQ5),3) round(mean(arrayLoudness),3)
 
-tableStats = [["READING", "MIN", "MEAN", "MEDIAN", "MAX"], ["Loudness", minLoudness, signalLoudness, maxLoudness], ["MQ5", round(mindMQ5, 3), round(densityMQ5, 3), round(mediandMQ5, 3), round(maxdMQ5, 3)]]
+tableStats = [["READING", "MIN", "MEAN", "MEDIAN", "MAX"], ["Loudness", minLoudness, signalLoudness, maxLoudness], ["MQ5", round(minMQ5, 3), round(densityMQ5, 3), round(medianMQ5, 3), round(maxMQ5, 3)]]
 table = AsciiTable(tableStats) #["Temperature", min(arrayTemperature), round(mean(arrayTemperature),3), max(arrayTemperature)], ["Humidity", min(arrayHumidity), round(mean(arrayHumidity),3), max(arrayHumidity)], 
 print table.table
