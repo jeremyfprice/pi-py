2e4c076ea8be89859c7c2ad3795525ffc8866519
Jeremy Price <jfprice@iupui.edu>
2017-10-25 09:04:30 -0400

    Add files via upload

--- sensor_test2.py
+++ sensor_test2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import random, time, grovepi
-from statistics import mean
+import statistics
 from terminaltables import AsciiTable
 
-timeout = time.time() + 60*2 # Set to run for 2 minutes
+timeout = time.time() + 60*0.5 # Set to run for .5 minutes
 
 sensorLoudness = 1
 sensorMQ5 = 2
@@ -57,8 +57,8 @@ minMQ5 = min(arrayMQ5)
 maxMQ5 = max(arrayMQ5)
 densityMQ5 = round(meanMQ5/1024, 6)
 signalLoudness = round((mean(arrayLoudness))/1024, 6)
-minLoudness = round((min(arrayLoudness))/1024, 6)
-maxLoudness = round((max(arrayLoudness))/1024, 6)
+minLoudness = min(arrayLoudness)/1024
+maxLoudness = max(arrayLoudness)/1024
 
 mindMQ5 = round(min(arrayMQ5)/1024, 6)
 maxdMQ5 = round(max(arrayMQ5)/1024, 6)
@@ -74,6 +74,6 @@ maxdMQ5 = round(max(arrayMQ5)/1024, 6)
 #print " "
 #round(mean(arrayMQ5),3) round(mean(arrayLoudness),3)
 
-tableStats = [["READING", "MIN", "MEAN", "MAX"], ["Loudness", min(arrayLoudness), signalLoudness, max(arrayLoudness)], ["MQ5", mindMQ5, densityMQ5, maxdMQ5]]
+tableStats = [["READING", "MIN", "MEAN", "MAX"], ["Loudness", minLoudness, signalLoudness, maxLoudness], ["MQ5", minMQ5, densityMQ5, maxMQ5]]
 table = AsciiTable(tableStats) #["Temperature", min(arrayTemperature), round(mean(arrayTemperature),3), max(arrayTemperature)], ["Humidity", min(arrayHumidity), round(mean(arrayHumidity),3), max(arrayHumidity)], 
 print table.table
